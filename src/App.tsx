cbdb5cfa487c9857dd611d8a01b536b7fde429df
kkata <14849535+kkata@users.noreply.github.com>
2022-12-22 21:50:35 +0900

    When

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { TodoInput } from "./Todo/TodoInput";
 import { TodoList } from "./Todo/TodoList";
 import styles from "./App.module.css";
-import { action, autorun, observable, reaction, runInAction } from "mobx";
+import { action, autorun, observable, reaction, runInAction, when } from "mobx";
 import { observer, useLocalObservable } from "mobx-react-lite";
 import { useEffect } from "react";
 import { useStore } from "./stores";
@@ -10,25 +10,11 @@ const App = () => {
   const { todos } = useStore();
 
   useEffect(() => {
-    const disposeReaction = reaction(
-      () => {
-        return {
-          length: todos.list.length,
-          unfinishedTodos: todos.unfinishedTodos,
-        };
-      },
-      (newValue, oldValue) => {
-        console.log({ newValue, oldValue });
-      },
-      {
-        delay: 2000,
-        onError: (err) => console.log(err.message),
-      }
-    );
+    const promiseWhen = when(() => !appUI.todoVisible);
 
-    return () => {
-      disposeReaction();
-    };
+    promiseWhen.then(() => {
+      console.log("clean up");
+    });
   }, []);
 
   const appUI = useLocalObservable(() => ({
