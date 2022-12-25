5505a0934ba29acebeb942f31f1766b2e8546ea3
kkata <14849535+kkata@users.noreply.github.com>
2022-12-25 20:59:06 +0900

    logging observables

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import { TodoInput } from "./Todo/TodoInput";
 import { TodoList } from "./Todo/TodoList";
 import styles from "./App.module.css";
-import { action, autorun, observable, reaction, runInAction, when } from "mobx";
+import {
+  action,
+  autorun,
+  observable,
+  reaction,
+  runInAction,
+  toJS,
+  when,
+} from "mobx";
 import { observer, useLocalObservable } from "mobx-react-lite";
 import { useEffect } from "react";
 import store, { useStore } from "./stores";
@@ -23,6 +31,10 @@ const App = observer(({ todos }: { todos: typeof store.todos }) => {
       this.todoVisible = !this.todoVisible;
     },
   }));
+
+  // debugging kind of things
+  // console.log(JSON.parse(JSON.stringify(todos.list)));
+  console.log(toJS(todos.list)); // This is expensive and should only ever be used when really needed
 
   return (
     <div className="App">
